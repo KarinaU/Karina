f10c8ae2e9997d186b9259e289619570a0aa85bd
Karina Likhosherstova <k.umyarova@gmail.com>
2015-10-11 11:35:32 +0300

    Lesson 9

---
+++ src/Girl/Girl.java
@@ -0,0 +1,46 @@
+package Girl;
+
+/**
+ * Created by IT_School on 11.10.2015.
+ */
+public class Girl {
+    private String fullName;
+    private int age;
+
+    public Girl (String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toEat (String restoran){
+        if(restoran.equals("Macdonalds"))
+            return ("No");
+        else return "Lets go";
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Girl" +
+                "Name='" + fullName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
